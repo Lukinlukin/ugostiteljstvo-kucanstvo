4cdd8e01b7b9c01e30e2c8bd67970a2d0de1d73c
Liletrin <liletrin@gmail.com>
2017-02-24 23:46:03 +0100

    GA code Contact form

---
+++ config/task-config.js
@@ -0,0 +1,61 @@
+module.exports = {
+  browserSync: {
+    server: {
+      baseDir: "public"
+    }
+  },
+
+  javascripts: {
+    entries: {
+      app: ["./app.js"]
+    },
+    extensions: ["js", "json"],
+    extractSharedJs: false
+  },
+
+  stylesheets: {
+    autoprefixer: {
+      browsers: ["last 3 version"]
+    },
+    sass: {
+      indentedSyntax: flase,
+      includePaths: [
+        "./node_modules/normalize.css"
+      ]
+    },
+    extensions: ["sass", "scss", "css"]
+  },
+
+  html: {
+    dataFile: "data/global.json",
+    htmlmin: {
+      collapseWhitespace: true
+    },
+    extensions: ["html", "json"],
+    excludeFolders: ["layouts", "shared", "macros", "data"]
+  },
+
+  images: {
+    extensions: ["jpg", "png", "svg", "gif"]
+  },
+
+  fonts: {
+    extensions: ["woff2", "woff", "eot", "ttf", "svg"]
+  },
+
+  static: true,
+
+  svgSprite: {
+    extensions: ["svg"]
+  },
+
+  production: {
+    rev: true
+  },
+
+  watch: {
+    gulpWatch: {
+      usePolling: false
+    }
+  }
+}
